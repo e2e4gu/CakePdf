24fbad439abaa157b2ead8c6b4b330277eb1c748
Minenko Konstantin <konstantin.minenko@office.e2e4gu.ru>
2016-05-30 17:48:05 +0600

    add source-sans-pro font; remove header black line;

--- src/Pdf/Engine/TcpdfEngine.php
+++ src/Pdf/Engine/TcpdfEngine.php
@@ -14,12 +14,16 @@ class TcpdfEngine extends AbstractPdfEngine
         //TCPDF often produces a whole bunch of errors, although there is a pdf created when debug = 0
         //Configure::write('debug', 0);
         $TCPDF = new \TCPDF($this->_Pdf->orientation(), 'mm', $this->_Pdf->pageSize());
+        $TCPDF->SetPrintHeader(false);
+        $TCPDF->SetPrintFooter(false);
         $TCPDF->AddPage();
         // set default font subsetting mode
         $TCPDF->setFontSubsetting(true);
 
         // set font
-        $TCPDF->SetFont('freeserif', '', 12);
+        $pathToFontDir = \Cake\Core\Plugin::path('CakePdf') . 'webroot' . DS . 'fonts' . DS . 'tcpdf' . DS;
+        $TCPDF->AddFont('source-sans-pro', '', $pathToFontDir . 'source-sans-pro.php');
+        $TCPDF->SetFont('source-sans-pro', '', 12);
         $TCPDF->writeHTML($this->_Pdf->html());
         return $TCPDF->Output($filename, $dest);
     }
